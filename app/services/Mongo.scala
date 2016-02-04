c6c319140ede7b9a2caf3b24af590bd4e592bead
BattleBeaver <kuzmentsov@gmail.com>
2016-02-05 01:27:11 +0200

    Mongo config for heroku

--- app/services/Mongo.scala
+++ app/services/Mongo.scala
@@ -1,7 +1,8 @@
 package services
 
 import com.google.inject.{Inject, Singleton}
-import com.mongodb.casbah.{MongoDB, MongoConnection}
+import com.mongodb.ServerAddress
+import com.mongodb.casbah._
 import play.Logger
 import play.api.Configuration
 import play.api.inject.ApplicationLifecycle
@@ -14,14 +15,26 @@ import scala.concurrent.Future
 @Singleton
 class Mongo @Inject()(applicationLifecycle: ApplicationLifecycle, configuration: Configuration) {
 
+  val path = "mongodb://beaver:wai3Ughi@ds045521.mongolab.com:45521/cingulata"
+  val uri = MongoClientURI(path)
+  val server = new ServerAddress(configuration.getString("mongo.db.host").get, configuration.getInt("mongo.db.port").get)
+
+  val credentials = MongoCredential.createCredential(
+    configuration.getString("mongo.db.username").get,
+    "admin",
+    configuration.getString("mongo.db.password").get.toCharArray
+  )
+
+  //val client = MongoClient(List(server), List(credentials))
+  val client = MongoClient(uri)
   //init of connection
-  val mongoConn = MongoConnection(configuration.getString("mongo.db.host").get, configuration.getInt("mongo.db.port").get)
+  //val mongoConn = MongoConnection(configuration.getString("mongo.db.host").get, configuration.getInt("mongo.db.port").get)
 
   //selecting collection from properties
-  val collection: MongoDB = mongoConn(configuration.getString("mongo.db.name").get)
+  val collection: MongoDB = client(configuration.getString("mongo.db.name").get)
 
   applicationLifecycle.addStopHook(() => {
     Logger.warn("Closing Mongo connection")
-    Future.successful(mongoConn.close())
+    Future.successful(client.close())
   })
 }
