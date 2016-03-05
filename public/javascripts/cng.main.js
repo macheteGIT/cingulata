aedaaed2bb2625147d2e59f81878c72a7e2c20aa
BattleBeaver <kuzmentsov@gmail.com>
2016-03-05 02:05:56 +0200

    Code style improvements.

--- public/javascripts/cng.main.js
+++ public/javascripts/cng.main.js
@@ -19,7 +19,7 @@
         if (this.readyState === 4 && this.status === 200) {
           callback(this);
         }
-      }
+      };
       xhttp.send();
     }
 
@@ -60,59 +60,12 @@
         if (!form || form.nodeName !== "FORM") {
             return;
         }
-        var i, j, q = [];
+        var i, q = [];
         for (i = form.elements.length - 1; i >= 0; i = i - 1) {
             if (form.elements[i].name === "") {
                 continue;
             }
-            switch (form.elements[i].nodeName) {
-                case 'INPUT':
-                    switch (form.elements[i].type) {
-                        case 'text':
-                        case 'hidden':
-                        case 'password':
-                        case 'button':
-                        case 'reset':
-                        case 'submit':
-                            q.push(form.elements[i].name + "=" + encodeURIComponent(form.elements[i].value));
-                            break;
-                        case 'checkbox':
-                        case 'radio':
-                            if (form.elements[i].checked) {
-                                q.push(form.elements[i].name + "=" + encodeURIComponent(form.elements[i].value));
-                            }
-                            break;
-                    }
-                    break;
-                case 'file':
-                    break;
-                case 'TEXTAREA':
-                    q.push(form.elements[i].name + "=" + encodeURIComponent(form.elements[i].value));
-                    break;
-                case 'SELECT':
-                    switch (form.elements[i].type) {
-                        case 'select-one':
-                            q.push(form.elements[i].name + "=" + encodeURIComponent(form.elements[i].value));
-                            break;
-                        case 'select-multiple':
-                            for (j = form.elements[i].options.length - 1; j >= 0; j = j - 1) {
-                                if (form.elements[i].options[j].selected) {
-                                    q.push(form.elements[i].name + "=" + encodeURIComponent(form.elements[i].options[j].value));
-                                }
-                            }
-                            break;
-                    }
-                    break;
-                case 'BUTTON':
-                    switch (form.elements[i].type) {
-                        case 'reset':
-                        case 'submit':
-                        case 'button':
-                            q.push(form.elements[i].name + "=" + encodeURIComponent(form.elements[i].value));
-                            break;
-                    }
-                    break;
-            }
+            q.push(form.elements[i].name + "=" + encodeURIComponent(form.elements[i].value));
         }
         return q.join("&");
     }
